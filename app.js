8f7edc94ffe4a9b258f253f38bd305bea45b15de
Chamitha <chamithashelan@gmail.com>
2024-05-19 16:21:21 +0530

    Created functions for basic operations.

---
+++ app.js
@@ -0,0 +1,20 @@
+let firstNum;
+let secondNum;
+let operator;
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
