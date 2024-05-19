3f20567c3edc667dd707639c307e55565158b089
Chamitha <chamithashelan@gmail.com>
2024-05-19 16:24:48 +0530

    Created function operate() :-     Takes the operator, First number, Second Number     as arguments and calls the required function     for the operation.

--- app.js
+++ app.js
@@ -18,3 +18,19 @@ function divide(a, b){
     return a / b;
 }
 
+function operate(operator, num1, num2){
+    switch(operator){
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+    }
+}
