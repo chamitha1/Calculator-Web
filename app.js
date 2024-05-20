f4caf64808757b7e9f6498977c2f8ad8df5fb40a
Chamitha <chamithashelan@gmail.com>
2024-05-20 09:49:12 +0530

    - Implemented basic functions ( + / - *). - Created a evaluate function to capture the   2 numbers and operator. - Created operate function to figure out which   operation to perform. - Implemented a functioning display

--- app.js
+++ app.js
@@ -2,6 +2,31 @@ let firstNum;
 let secondNum;
 let operator;
 
+
+const btnContainer = document.querySelector(".btn-container");
+const display = document.querySelector(".display");
+const btnClear = document.querySelector(".btn-clear")
+
+let displayContent = "";
+
+
+btnContainer.addEventListener("click",(evt)=>{
+    if(evt.target == btnContainer ){
+        displayContent +="";
+    }else{
+        displayContent += `${evt.target.textContent}`;
+    }
+
+    if (evt.target.textContent == "AC") displayContent = "";
+    
+    if (evt.target.textContent == "="){
+        result = evaluate(displayContent);
+        displayContent = result;
+    } 
+
+    display.textContent = displayContent;
+});
+
 function add(a, b){
     return a + b;
 }
@@ -19,39 +44,37 @@ function divide(a, b){
 }
 
 function operate(operator, num1, num2){
-    switch(operator){
-        case '+':
-            add(num1, num2);
-            break;
-        case '-':
-            subtract(num1, num2);
-            break;
-        case '*':
-            multiply(num1, num2);
-            break;
-        case '/':
-            divide(num1, num2);
-            break;
-    }
+    if(operator == '+') return num1+num2;
+    if(operator == '-') return num1-num2;
+    if(operator == '*') return num1*num2;
+    if(operator == '/') return num1/num2;
+    
+    
 }
 
-const btnContainer = document.querySelector(".btn-container");
-const display = document.querySelector(".display");
-const btnClear = document.querySelector(".btn-clear")
+function isOperator(val){
+    switch(val){
+        case '+':
+            return true;
+        case '-':
+            return true;
+        case '*':
+            return true;
+        case '/':
+            return true;
+         default:
+            return false;
+        }
+}
+function evaluate(str){
+    let numAr = str.split(/[^0-9\" \"]/g);
+    console.log(numAr);
+    let firstNum = Number(numAr[0]);
+    let secondNum = Number(numAr[1]);
+    console.log(firstNum +" " + secondNum);
+    let operator = str.split("")
+                      .filter((x)=> isOperator(x));
+    console.log(operator);
+    return operate(operator,firstNum,secondNum); //Returns the result of operation
 
-let displayContent = "";
-
-
-btnContainer.addEventListener("click",(evt)=>{
-    if(evt.target == btnContainer ){
-        displayContent +="";
-    }else{
-        displayContent += ` ${evt.target.textContent}`;
-    }
-
-    if (evt.target.textContent == "AC") displayContent = "";
-
-    display.textContent = displayContent;
-});
-
-
+}
